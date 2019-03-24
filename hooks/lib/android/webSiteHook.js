12e932a0b059f4326773b8b5b99988e5d283a9c8
moisisv <39265508+moisisv@users.noreply.github.com>
2019-03-24 23:54:04 +0200

    Update webSiteHook.js
    Phonegap support

--- hooks/lib/android/webSiteHook.js
+++ hooks/lib/android/webSiteHook.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 var path = require('path');
 var mkpath = require('mkpath');
 var ConfigXmlHelper = require('../configXmlHelper.js');
-var WEB_HOOK_FILE_PATH = path.join('ul_web_hooks', 'android', 'android_web_hook.html');
+var WEB_HOOK_FILE_PATH = path.join('www','ul_web_hooks', 'android', 'android_web_hook.html');
 var WEB_HOOK_TPL_FILE_PATH = path.join('plugins', 'cordova-universal-links-plugin', 'ul_web_hooks', 'android_web_hook_tpl.html');
 var LINK_PLACEHOLDER = '[__LINKS__]';
 var LINK_TEMPLATE = '<link rel="alternate" href="android-app://<package_name>/<scheme>/<host><path>" />';
